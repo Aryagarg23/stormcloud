f70cbe6bc102ca093a2bcd5a4cf4580991203874
root <root@GPU-BOX.localdomain>
2026-09-04 08:01:09 -0700

    fix: allow credentialed browser authentication

--- src/stormcloud/main.py
+++ src/stormcloud/main.py
@@ -30,7 +30,7 @@ app.include_router(grading_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origin_list,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type", "Idempotency-Key", "X-Request-ID"],
     expose_headers=["X-Request-ID"],
